3217771a42f79a98fa25612c7f4df5ed082ac998
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-12-05 16:19:05 +0000

    Only log if console present

--- tests/hash-change.js
+++ tests/hash-change.js
@@ -2,7 +2,7 @@ location.hash = '';
 var expected = [];
 var lastDone;
 function update(token) {
-  if (!window.mochaPhantomJS) console.log(token);
+  if (!window.mochaPhantomJS && console && typeof console.log == 'function') console.log(token);
   if (!expected.length) {
     throw new Error('The route ' + token + ' was not expected.');
   }
